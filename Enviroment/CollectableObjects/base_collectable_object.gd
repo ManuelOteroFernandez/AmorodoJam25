e9ffb2b84797da1c35c86669e95841b52c0b3658
Manuel Otero <manuelOtero.af@gmail.com>
2025-05-15 20:37:36 +0200

    Collectable objects system

---
+++ Enviroment/CollectableObjects/base_collectable_object.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@export var value: int = 1
+@export var type: Collectable.COLLECTABLE_TYPE = Collectable.COLLECTABLE_TYPE.BREZO
+
+
+func _ready() -> void:
+	Collectable.register_collectable_value(type,value)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if not body.is_in_group("player"):
+		return
+	
+	Collectable.add_collectable_object(type,value)
