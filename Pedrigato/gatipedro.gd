57176baa57b56ce9d06951a4fe22d5a9ca2b4942
Manuel Otero <manuelOtero.af@gmail.com>
2025-05-18 08:48:37 +0200

    [FIX] Gatipedro double player detected

--- Pedrigato/gatipedro.gd
+++ Pedrigato/gatipedro.gd
@@ -3,11 +3,14 @@ extends Node2D
 @export var distance:float
 @export var time:float
 
+var is_used = false
+
 func _ready() -> void:
 	$AnimatedSprite2D.flip_h = distance > 0
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and not is_used:
+		is_used = true
 		var goal = global_position.x + distance
 		var final_val = Vector2(goal,global_position.y)
 		$AnimatedSprite2D.play("default")
